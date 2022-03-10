b6299fe2b7632b90334a5c5378e71a836e7cbf43
adriancro8888 <adriancro8888@gmail.com>
2022-03-10 03:10:55 +0200

    setting view controllers

---
+++ BuyIN/Controllers/CategoriesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategoriesViewController.swift
+//  BuyIN
+//
+//  Created by apple on 3/10/22.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
