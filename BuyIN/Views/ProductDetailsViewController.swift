aa8c60405aee5795a567686941592617c7f53f9a
adriancro8888 <adriancro8888@gmail.com>
2022-03-12 09:13:31 +0200

    Adding ProductDetailsViewController

---
+++ BuyIN/Views/ProductDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProductDetailsViewController.swift
+//  BuyIN
+//
+//  Created by Amr Hossam on 12/03/2022.
+//
+
+import UIKit
+
+class ProductDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
