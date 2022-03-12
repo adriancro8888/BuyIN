9451b0773f9b8822630e059c6c197e8098cf6518
adriancro8888 <adriancro8888@gmail.com>
2022-03-12 15:24:48 +0200

    Product screen

--- BuyIN/Controllers/CategoriesViewController.swift
+++ BuyIN/Controllers/CategoriesViewController.swift
@@ -175,6 +175,15 @@ extension CategoriesViewController :UICollectionViewDelegate{
 
 
         }
+        else if collectionView == productsCollectonView{
+            let newViewController = ProductsViewController(nibName: "ProductsViewController", bundle: nil)
+            let collection = self.selectedCollection!
+            newViewController.selectedCollection = collection
+           
+            
+            // Present View "Modally"
+            self.present(newViewController, animated: true, completion: nil)
+        }
     }
     
 }
